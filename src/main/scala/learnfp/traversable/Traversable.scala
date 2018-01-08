2feae1428b93cbd47959b48016cc77137f8f8223
jiaming.shang <jiaming.shang@rea-group.com>
2018-01-08 22:50:30 +0800

    implement traverse

--- src/main/scala/learnfp/traversable/Traversable.scala
+++ src/main/scala/learnfp/traversable/Traversable.scala
@@ -7,8 +7,20 @@ class TraversableOps[A, F[_]](initialXs:List[F[A]])(implicit functor: Functor[F]
   import learnfp.functor.FunctorOps._
   import learnfp.applicative.ApplicativeOps._
 
-  def traverse[B](fx:A => B)(implicit applicative: Applicative[F]):F[List[B]] = ???
-  def sequence(implicit applicative: Applicative[F]):F[List[A]] = ???
+  def traverse[B](fx:A => B)(implicit applicative: Applicative[F]):F[List[B]] = {
+    initialXs.foldLeft(applicative.pure(List[B]()))((acc:F[List[B]],x:F[A])=>{
+      val v:F[List[B]] = functor.fmap(x)(y=>List(fx(y)))
+
+      def func(x:List[B])(y:List[B]) = x:::y
+
+      val Ffunc = functor.fmap(acc)(x=>func(x)(_))
+
+      applicative.<*>(Ffunc)(v)
+    })
+  }
+  def sequence(implicit applicative: Applicative[F]):F[List[A]] = {
+    traverse(identity)
+  }
 }
 
 object TraversableOps {
