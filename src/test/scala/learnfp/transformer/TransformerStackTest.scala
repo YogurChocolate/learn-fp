39bc97b82781d53353c8b6fbf2a662cc735215ca
jiaming.shang <jiaming.shang@rea-group.com>
2018-01-11 16:52:35 +0800

    fix monad transformer stack test

--- src/test/scala/learnfp/transformer/TransformerStackTest.scala
+++ src/test/scala/learnfp/transformer/TransformerStackTest.scala
@@ -17,18 +17,32 @@ class TransformerStackTest extends WordSpecLike with Matchers {
     type OuterT[A] = WriterT[A, Maybe, List[String]]
     type App[A] = StateT[String, OuterT, A];
 
+    def mtell(w:List[String]):App[Unit] ={
+       StateT.lift[String,OuterT,Unit](tell[Maybe,List[String]](w))
+    }
+
+    def mput(w:String):App[Unit] ={
+       putT[String,OuterT](w)
+    }
+
     "work" in {
       {
         for {
           x <- 10.pure[App]
           // TODO: tell "een"
+          _ <- mtell(List("een"))
           // TODO: put state "one"
+          _ <- mput("one")
           y <- 20.pure[App]
           // TODO: tell "twee"
+          _ <- mtell(List("twee"))
           // TODO: put state "two"
+          _ <- mput("two")
           z <- 30.pure[App]
           // TODO: tell "drie"
+          _ <- mtell(List("drie"))
           // TODO: put state "three"
+          _ <- mput("three")
         } yield x
       }.runStateT("null").runWriterT() match {
         case Just(v) => v shouldBe (List("een", "twee", "drie"), ("three", 10))
